28bdb50f87c0bccd2fe2c9d0db784ca3ee71e556
xuyang zhao <771906409@qq.com>
2022-01-11 11:32:44 +0800

    add timer to evaluate enclave time

--- src/extension/include/extension_helper.hpp
+++ src/extension/include/extension_helper.hpp
@@ -1,3 +1,6 @@
 #pragma once
+
+#include <cstdio>
 void print_info(const char *fmt,... );
 void print_error(const char *fmt,...);
+
--- src/extension/interface/interface.cpp
+++ src/extension/interface/interface.cpp
@@ -2,6 +2,7 @@
 #include <tee_interface.hpp>
 #include <sync.h>
 #include <extension_helper.hpp>
+#include <timer.hpp>
 TEEInvoker *TEEInvoker::invoker = NULL;
 TEEInvoker::~TEEInvoker() {
     freeBuffer(req_buffer);
@@ -14,19 +15,35 @@ int TEEInvoker::sendRequest(Request *req) {
     BaseRequest *req_control = static_cast<BaseRequest *>(req_buffer);
     /* TODO write barrier */
     // print_info("REQUIEST sent");
+
+    // static myTime time;
+	// static double timer = 0,duration = 0;
+	// time.tic();
+
     req_control->status = SENT;
 
     /* wait for status */
-     while (req_control->status != DONE)
-         YIELD_PROCESSOR;
-     /* TODO read barrier */
+    while (req_control->status != DONE)
+        YIELD_PROCESSOR;
+    /* TODO read barrier */
 
-     req->copyResultFrom(req_buffer);
-     resp = req_control->resp;
+    req->copyResultFrom(req_buffer);
+    resp = req_control->resp;
 
-     /* read-write barrier, no read move after this barrier, no write move before this barrier */
-     req_control->status = NONE;
-     return resp;
+    /* read-write barrier, no read move after this barrier, no write move before this barrier */
+    req_control->status = NONE;
+    
+    // time.toc();
+    // duration = time.getDuration() / 1000000;
+    // timer += duration;
+    // if(req_control->reqType == CMD_FLOAT_DEC || req_control->reqType == CMD_INT_DEC){
+    //     print_info("shit");
+    //     char ch[1000];	
+    //     sprintf(ch, "total duration in ms: sum %f ", timer );
+    //     print_info(ch);
+    // }
+
+    return resp;
 }
 
 TEEInvoker::TEEInvoker() {
