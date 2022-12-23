8bc35980aef62784ffd8ac5dc96eff60ff102881
xuyang zhao <brokenbones@sjtu.edu.cn>
2022-12-23 22:33:04 +0800

    delete useless files

--- load_tpch.sh
+++
@@ -1,5 +0,0 @@
-cd benchmark 
-psql -U postgres -d test  -f db_schemas/tpch-schema-encrypted.sql
-psql -U postgres -d test  -f db_schemas/tpch-index.sql  
-java -Dlog4j.configuration=log4j.properties -jar bin/tpch.jar -b tpch -o output -s 30 --config config/tpch_config.xml --load true --execute false
-cd .. 
--- test/tpch/run.sh
+++ test/tpch/run.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-run_tag=$1
+target=$1
+run_tag=$2
 
 if [ ! -n "$run_tag" ] ; then
     echo "you should input run_tag to describe current run"
 fi
+
 mkdir -p $run_tag
 
 PG_LOCAL=1
 pg_ports=5432
-pg_ip=localhost
+pg_ip=192.168.12.21
 
-sqls=`ls sqls/*.sql`
+sqls=`ls $target/*.sql`
 for sql in $sqls
-do 
-    number=`echo $sql | awk -F "." '{print $1}' | awk -F "/" '{print $2}'`
+do
+    number=`echo $sql | awk -F "." '{print $1}' | awk -F "/" '{print $3}'`
     output_file=$run_tag/$number.out
-    echo "$sql > $output_file"
-    # ls ../../test.binlog -lh > $output_file
-    psql -U postgres -h $pg_ip -p $pg_ports -d test -f $sql > $output_file
-    #psql -U postgres -d test -f $sql > $output_file
+
+    psql -U postgres -d test -h $pg_ip -p $pg_ports -f $sql > $output_file
+    querytime=`cat $output_file | grep "Time:" | awk '{print $2}'`
+    echo $querytime
     sleep 3s
+
+    ## run three times
+    # total=0
+    # for n in 1 2 3
+    # do
+    #     sleep 3s
+    #     psql -U postgres -d test -h $pg_ip -p $pg_ports -f $sql > $output_file
+    #     timeStr=`cat $output_file | grep "Time:" | awk '{print $2}'`
+    #     total=`echo "$timeStr + $total"| bc -l`
+    # done
+
+    # result=`echo "$total/3" | bc -l`
+    # echo $result
+    # sleep 3s
 done
