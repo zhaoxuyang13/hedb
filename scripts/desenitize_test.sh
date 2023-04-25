63a89025a120dda2fe7837397d36360f6f27800f
Xuyang Zhao <771906409@qq.com>
2023-04-25 09:25:34 +0800

    minor fix on desenitize_test.hsh

--- scripts/desenitize_test.sh
+++ scripts/desenitize_test.sh
@@ -18,7 +18,8 @@ rm $OUTPUT_FILE
 touch $OUTPUT_FILE
 # docker run --entrypoint /home/klee/entrypoint-min.sh -v $LOG_PATH:/home/klee/001-log -v $OUTPUT_FILE:/home/klee/output.log zhaoxuyang13/klee-desen:1.0
 # docker run --entrypoint /home/klee/entrypoint-without-one.sh -v $LOG_PATH:/home/klee/001-log -v $OUTPUT_FILE:/home/klee/output.log zhaoxuyang13/klee-desen:1.0
-docker run --entrypoint /home/klee/entrypoint-min.sh -v $OUTPUT_FILE:/home/klee/output.log  zhaoxuyang13/klee-desen:1.0
+# logs is built in docker image, for simplicity 
+docker run --entrypoint /home/klee/entrypoint.sh -v $OUTPUT_FILE:/home/klee/output.log  zhaoxuyang13/klee-desen:1.0
 
 # filter out time, and compute and convert (h:mm:ss or m:ss) to seconds
 cat $OUTPUT_FILE | grep "Elapsed (wall clock) time (h:mm:ss or m:ss):" \
