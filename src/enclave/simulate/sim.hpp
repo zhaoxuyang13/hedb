fafad21e3370021c9861dff4396ef0a2454877ed
Xuyang Zhao <771906409@qq.com>
2023-05-29 09:54:44 +0800

    change to local setting, alter sql scripts

--- src/enclave/simulate/sim.hpp
+++ src/enclave/simulate/sim.hpp
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-#define MAX_DECRYPT_THREAD 10
+#define MAX_DECRYPT_THREAD 1
 
 int encrypt_bytes(uint8_t *pSrc, size_t src_len, uint8_t *pDst, size_t exp_dst_len);
 int decrypt_bytes(uint8_t *pSrc, size_t src_len, uint8_t *pDst, size_t exp_dst_len);
