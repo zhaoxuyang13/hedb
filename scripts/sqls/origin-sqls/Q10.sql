9e93bef28a7ca148442af3cd668114b624bd9a59
Xuyang Zhao <771906409@qq.com>
2023-06-05 12:43:20 +0800

    fix sqls

--- scripts/sqls/origin-sqls/Q10.sql
+++ scripts/sqls/origin-sqls/Q10.sql
@@ -8,7 +8,7 @@ where
 c_custkey = o_custkey
 and l_orderkey = o_orderkey
 and o_orderdate >= date '1993-08-01' 
-and o_orderdate < date '1993-08-01' + interval '3' month 
+and o_orderdate < date '1993-11-01'
 and l_returnflag = 'R' 
 and c_nationkey = n_nationkey
 group by
